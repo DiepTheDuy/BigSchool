97fa400d45f4fd5a58567cbf6d11df9a2fcf973e
TheDuy <dieptheduy2000@gmail.com>
2021-07-09 10:56:02 +0700

    page 34

---
+++ BigSchool/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BigSchool.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BigSchool.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(BigSchool.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
